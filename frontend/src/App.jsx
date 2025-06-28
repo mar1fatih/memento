52addf5145681105d2337aafab7747f42af2ec95
mar1fatih <marouanefatih631@gmail.com>
2025-06-28 15:47:24 +0100

    adding some styling to header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './components/Register/register.jsx';
 import Login from './components/Login/login.jsx';
 import Gallery from './components/Gallery/gallery.jsx';
@@ -10,16 +10,16 @@ import './App.css'
 function App() {
   return (
     <>
-    <Header />
-    <Router>
+    <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/upload" element={<Upload />} />
       </Routes>
-    </Router>
-    <Footer />
+      <Footer />
+    </BrowserRouter>
     </>
   )
 }
