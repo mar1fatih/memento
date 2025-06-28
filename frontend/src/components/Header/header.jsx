3b1c37b6e9dfabbedd9af55cf49d0b6e6c6d9b86
mar1fatih <marouanefatih631@gmail.com>
2025-06-28 15:19:07 +0100

    adding components + modiying header component

---
+++ frontend/src/components/Header/header.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import './header.css';
+
+function Header() {
+  const logout = () => {
+    localStorage.removeItem('token');
+    window.location.href = '/login';
+  };
+
+  return (
+    <div className='header'>
+        <div className='memento-logo'></div>
+        <nav>
+            <Link to="/register">Register</Link> |{" "}
+            <Link to="/login">Login</Link> |{" "}
+            <Link to="/gallery">Gallery</Link> |{" "}
+            <Link to="/upload">Upload</Link> |{" "}
+            <button onClick={logout}>Logout</button>
+        </nav>
+    </div>
+  );
+}
+
+export default Header;
---
+++ frontend/src/components/Upload/upload.jsx
@@ -0,0 +1,7 @@
+function Upload() {
+  return (
+    <></>
+  );
+}
+
+export default Upload;
