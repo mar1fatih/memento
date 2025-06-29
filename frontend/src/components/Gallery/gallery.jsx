50a2d3903f52cfed7a9303865dabba66b38638ce
mar1fatih <marouanefatih631@gmail.com>
2025-06-29 16:31:16 +0100

    adding /gellery route

--- frontend/src/components/Gallery/gallery.jsx
+++ frontend/src/components/Gallery/gallery.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react';
+import API from '../../api/api.js';
+
 function Gallery() {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    API.get('/photos')
+      .then((res) => setPhotos(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <></>
+    <div>
+      <h2>Gallery</h2>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {photos.map((photo) => (
+          <img
+            key={photo._id}
+            src={photo.url}
+            alt=""
+            style={{ width: '200px', margin: '10px' }}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
