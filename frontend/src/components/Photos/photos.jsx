d8294c7185f8dfe30274d8dd16cf05c594a36ce7
mar1fatih <marouanefatih631@gmail.com>
2025-07-22 01:12:06 +0100

    adding images selection

--- frontend/src/components/Photos/photos.jsx
+++ frontend/src/components/Photos/photos.jsx
@@ -1,16 +1,20 @@
 import { useState, useEffect, useRef, useCallback } from 'react';
-import PhotoCard from '../PhotoCard/photoCard';
 import API from '../../api/api.js';
-import './photos.css'
+import './photos.css';
 
 function Photos() {
   const [photos, setPhotos] = useState([]);
+  const [selectedPhotos, setSelectedPhotos] = useState([]);
+  const [filteredSelect, setFileredSelect] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [hasMore, setHasMore] = useState(false);
   const [error, setError] = useState(false);
   const [page, setPage] = useState(1);
   const [totalPhotos, setTotalPhotos] = useState(0);
+  const [hovered, setHovered] = useState(null);
+  const [checkHovered, setCheckHovered] = useState(null);
+  const [selectionMode, setSelectionMode] = useState(false);
 
   const observer = useRef();
   const lastPhoto = useCallback(ele => {
@@ -22,6 +26,37 @@ function Photos() {
     });
     if (ele) observer.current.observe(ele);
   }, [isLoading, hasMore]);
+
+  const handlePhotosMouseEnter = (index) => {
+    setHovered(index);
+  }
+  const handlePhotosMouseLeave = () => {
+    setHovered(null);
+  }
+
+  const handleCheckPhotoMouseEnter = index => {
+    setHovered(index);
+    setCheckHovered(index);
+  }
+
+  const handleCheckPhotoMouseLeave = () => {
+    setCheckHovered(null);
+  }
+
+  const handleCheckPhotos = (photoId) => {
+    if (filteredSelect.includes(photoId)) {
+      if (filteredSelect.length > 1) {
+        setFileredSelect(prev => prev.filter(id => id !== photoId));
+      } else {
+        setFileredSelect([]);
+        setSelectedPhotos([]);
+        setSelectionMode(false);
+      }
+    } else {
+    setSelectedPhotos(prev => [...new Set([...prev, photoId])]);
+    }
+    console.log(typeof photoId);
+  }
 
   useEffect(() => {
     setError(false);
@@ -61,11 +96,78 @@ function Photos() {
     setHasMore(photos.length < totalPhotos);
   }, [photos.length]);
 
+  useEffect(() => {
+    if (selectedPhotos.length > 0) {
+      setSelectionMode(true);
+      setFileredSelect([...new Set([...selectedPhotos])]);
+      console.log(filteredSelect);
+    } else {
+      setSelectionMode(false);
+    }
+  }, [selectedPhotos]);
+
   return (
     <>
+        {selectionMode && (
+          <div 
+          style={{
+            position: 'fixed',
+            left: '50px',
+            top: '20px',
+            display: 'flex',
+          }}
+        ><div className='del-cancel-btn' 
+          onClick={() => {
+            setSelectionMode(false)
+            setFileredSelect([]);
+            setSelectedPhotos([]);
+          }}
+        ></div>
+        <p className='selected'>{filteredSelect.length}&nbsp;selected</p></div>
+        )}
+        <div>selected items: {filteredSelect.length}</div>
         <div style={{ display: 'flex', flexWrap: 'wrap' }}>
           {photos.map((photo, index) =>
-            <PhotoCard photo={photo} key={photo._id} />
+            <div
+            className='photos'
+            key={photo._id}
+            style={{
+                height: '230px',
+                margin: '10px',
+                width: `${((photo.width / photo.height) * 230)}px`
+            }}
+            onMouseEnter={() => handlePhotosMouseEnter(photo._id)}
+            onMouseLeave={() => handlePhotosMouseLeave(photo._id)}
+            >
+                <div className="select">
+                  <svg className='select-checked' style={{
+                    display: filteredSelect.includes(photo._id) ? 'block' : (selectionMode ? 'none' : (photo._id === hovered && 'block')),
+                    opacity: filteredSelect.includes(photo._id) && 1 || photo._id === checkHovered && 1 || photo._id === hovered && 0.5,
+                    fill: filteredSelect.includes(photo._id) && 'hsla(217, 95%, 83%, 1.00)',
+                    }}
+                    onMouseEnter={() => handleCheckPhotoMouseEnter(photo._id)}
+                    onMouseLeave={() => handleCheckPhotoMouseLeave(photo._id)}
+                    onClick={() => handleCheckPhotos(photo._id)}
+                  >
+                      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path>
+                  </svg>
+                  <svg
+                      className='select-not-checked'
+                      style={{ display: filteredSelect.includes(photo._id) ? 'none' : (selectionMode ? 'block' : 'none') }}
+                      onClick={() => setSelectedPhotos(prev => [...prev, photo._id])}
+                  >
+                      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path>
+                  </svg>
+                </div>
+                <div
+                    className='photos-img'
+                    style={{
+                      backgroundImage: `url(${photo.optimizedUrl || photo.url.replace('/upload/', '/upload/c_scale,h_230/')})`,
+                      filter: photo._id === hovered && 'brightness(0.7)',
+                    }}
+                >
+                </div>
+            </div>
           )}
         </div>
         <div className='last item' style={{ height: '30px'}} ref={lastPhoto}></div>
